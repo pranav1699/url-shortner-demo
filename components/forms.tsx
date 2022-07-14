bbac04e771ec485677b052c97a4d8756acecd3b4
pranav <pranavsadagopan@gmail.com>
2022-07-14 22:55:30 +0530

    added some styling and added two fields

--- components/forms.tsx
+++ components/forms.tsx
@@ -1,67 +1,99 @@
-import router, { Router ,useRouter} from "next/router";
+import router, { Router, useRouter } from "next/router";
 import React, { Component, useState } from "react";
 
-function Forms ({shortUrlOld}){
-  
-
+function Forms({ shortUrlOld }) {
   const [value, setValue] = useState("");
-  const [shortUrl, setShortUrl]= useState(shortUrlOld);
-  const handleChange=(event: any)=>{
-    setValue(event.target.value);
-  }
-
-  const search = async() => {
-    console.log()
-    const y ={url: value}
-    const postResult: any = await fetch(
-      "http://localhost:4000/url-generator/generate",
-      {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body:JSON.stringify(y)
-      }
-    ).then((res) => res.json());
-    
-    console.log(postResult);
-    await setShortUrl("http://localhost:3000/"+ postResult)
-    localStorage.setItem('x', "http://localhost:3000/"+ postResult)
-    console.log(localStorage.getItem('x'))
+  const [name, setName] = useState("");
+  const [type, setType] = useState("");
+  const [shortUrl, setShortUrl] = useState(shortUrlOld);
+  const handleChange = (event: any, setstate) => {
+    setstate(event.target.value);
   };
 
-    return (
-      <div>
-          <label>
-            URL:
-            <input
-              type="text"
-              value={value}
-              onChange={(e) => handleChange(e)}
-            />{" "}
-          </label>
-          <div className="m-3 bg-red-400 w-20 h-10 rounded-md">
-            <button className ="ml-4" type="submit" value="Submit" onClick={search} >Submit </button>
-          </div>
-          <a href={shortUrl}>{shortUrl}</a>
-      </div>
-    );
-  }
+  const search = async () => {
+    console.log();
+    const y = { url: value, name: name, type: type };
+    // const postResult: any = await fetch(
+    //   "http://localhost:4000/url-generator/generate",
+    //   {
+    //     method: "POST",
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     },
+    //     body:JSON.stringify(y)
+    //   }
+    // ).then((res) => res.json());
 
+    console.log(y);
+    // await setShortUrl("http://localhost:3000/"+ postResult)
+    // localStorage.setItem('x', "http://localhost:3000/"+ postResult)
+    console.log(localStorage.getItem("x"));
+  };
+
+  return (
+    <div className="flex flex-col">
+      
+        <label className="font-semibold text-gray-700">
+          URL
+          <input
+            className="border-2 rounded-md font-normal m-3 p-2"
+            placeholder="Enter the url"
+            type="text"
+            value={value}
+            onChange={(e) => handleChange(e, setValue)}
+          />{" "}
+        </label>
+      
+      
+        <label className="font-semibold text-gray-700">
+          Name
+          <input
+            className="border-2 rounded-md font-normal m-3 p-2"
+            placeholder="Enter the name"
+            type="text"
+            value={name}
+            onChange={(e) => handleChange(e, setName)}
+          />{" "}
+        </label>
+      
+        <label className="font-semibold text-gray-700">
+          Type
+          <input
+            className="border-2 rounded-md font-normal m-3 p-2"
+            placeholder="Enter the type"
+            type="text"
+            value={type}
+            onChange={(e) => handleChange(e, setType)}
+          />{" "}
+        </label>
+      
+      <div>
+        <button
+          className=" m-2 p-2 bg-red-400 rounded-md text-justify text-zinc-700"
+          type="submit"
+          value="Submit"
+          onClick={search}
+        >
+          Submit{" "}
+        </button>
+      </div>
+      <a href={shortUrl}>{shortUrl}</a>
+    </div>
+  );
+}
 
 export default Forms;
 
 export async function getServerSideProps(req: any) {
- 
   let x = localStorage.getItem("x");
-  console.log(x)
-console.log('old', x);
-if(x == undefined){
-  x=''
-}
-  return{
-    props:{
-      shortUrl: x
-    }
+  console.log(x);
+  console.log("old", x);
+  if (x == undefined) {
+    x = "";
   }
+  return {
+    props: {
+      shortUrl: x,
+    },
+  };
 }
